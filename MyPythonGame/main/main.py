dc3e9caf198cb4da773f6d27c8827754e7661816
guillaumesibot <guillaumesibot@gmail.com>
2019-01-04 11:57:45 +0100

    Exit by break & bug

--- MyPythonGame/main/main.py
+++ MyPythonGame/main/main.py
@@ -1,7 +1,5 @@
 #MyPythonGame Project For RSG
 
-import sys
-from sys import exit
 from random import *
 
 print("Hello, my name is Eve")
@@ -14,29 +12,18 @@ MinNum= randint(0, MaxRange)
 MaxNum = randint(99 + MinNum, 100 + MaxRange )
 SecretNum = randint(MinNum, MaxNum )
 
-""" DEBUG
-print(MinNum)
-print(MaxNum)
-print(SecretNum)
-"""
-
 print("I am thinking of a number between "+ str(MinNum) + " and " + str(MaxNum) + ", care to take a guess?")
-
 
 NumTry = 0
 MaxTry = 10
 
 for NumTry in range(0,MaxTry):
-    UserNum = input()
+    UserNum = int(input())              #int() important to compare int between int SecretNum radint()
     if UserNum == SecretNum:
         print("You guessed correctly!")
+        break
     else:
         print("You guessed wrong, sorry!")
     NumTry += 1
-print()  
-print("You had try " + str(NumTry) + " times")  
-
-#sys.exit()   
-#quit()
-#raise SystemExit
-#exit()
+print("    ")
+print("You had try " + str(NumTry) + " times")
