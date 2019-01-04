d50980646563bc6603b2a9a8303b8cb3d4f3c262
guillaumesibot <guillaumesibot@gmail.com>
2019-01-04 15:28:12 +0100

    Refactoring but Play again doesn't work

--- MyPythonGame/main/main.py
+++ MyPythonGame/main/main.py
@@ -1,36 +1,68 @@
 #MyPythonGame Project For RSG
 
 from random import *
-
-print("Hello, my name is Eve")
-print("What is your name ?")
-user_name = input()
-print("Hello "+ user_name)
-
-MaxRange = 20
-MinNum= randint(0, MaxRange)
-MaxNum = randint(99 + MinNum, 100 + MaxRange )
-SecretNum = randint(MinNum, MaxNum )
-
-print("I am thinking of a number between "+ str(MinNum) + " and " + str(MaxNum) + ", care to take a guess?")
-
-NumTry = 0
-MaxTry = 10
-
-for NumTry in range(0,MaxTry):
-    UserNum = int(input()) 
+def PlayAgain(play):
+    again=str(input("Do you want to play again, type yes or no "))
     try:
-        val = float(UserNum)
+       val = str(again)
     except ValueError:
-         print("That's not an number!")
-         print("No.. input string is not a float number. It's a string")
+        print("That's not response!")
+        print("Write Yes or Y or yes or y")
+    if again == "yes" or "YES" or "y" or "Y":
+       main()
+    if again == "no":
+       play = False
+       return play
 
-    if UserNum == SecretNum:
-        print("You guessed correctly!")
+
+
+
+
+
+def Intro():
+    print("Hello, my name is Eve")
+    print("What is your name ?")
+    user_name = input()
+    print("Hello "+ user_name)
+    MaxRange = 20
+    MinNum= randint(0, MaxRange)
+    MaxNum = randint(99 + MinNum, 100 + MaxRange )
+    SecretNum = randint(MinNum, MaxNum )
+
+    print("I am thinking of a number between "+ str(MinNum) + " and " + str(MaxNum) + ", care to take a guess?")
+    return SecretNum
+
+
+
+def Game(SecretNum):
+    play = True
+    while play:
+        NumTry = 0
+        MaxTry = 5
+
+        for NumTry in range(0,MaxTry):
+            UserNum = input()  
+            try:
+                val = int(UserNum)
+            except ValueError:
+                 print("That's not an number!")
+                 print("No.. input string is not a int number. It's a string")
+             
+            if UserNum == SecretNum:
+                print("You guessed correctly!")
+                break
+            else:
+               print("You guessed wrong, sorry!")
+               NumTry += 1
+        print("    ")
+        print("You had try " + str(NumTry) + " times")
+        PlayAgain(play)
         break
-    else:
-        print("You guessed wrong, sorry!")
-    NumTry += 1
-print("    ")
-print("You had try " + str(NumTry) + " times")
+
+                     
+def main():
+    Game(Intro())
+    
+
+main()
 
